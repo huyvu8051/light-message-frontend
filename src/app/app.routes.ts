3105e3a1f9ac7a21f8191014852e565d6b2a8474
huyvu8051 <huyvu8051@gmail.com>
2025-02-03 01:20:11 +0700

    main message frame

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router'
+import {SimpleAuthComponent} from './auth/simple-auth.component'
+import {MessageComponent} from './message/message.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth/:userId',
+    component: SimpleAuthComponent
+  },
+  {
+    path: 'message',
+    component: MessageComponent
+  }
+]
---
+++ src/app/auth/simple-auth.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core'
+import {ActivatedRoute, Router} from '@angular/router'
+
+@Component({
+  template: `
+    <h1>User id: {{ userId }}</h1>
+  `,
+  standalone: true
+})
+export class SimpleAuthComponent {
+  userId: Number = 0
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.userId = Number(this.route.snapshot.paramMap.get('userId'))
+    this.router.navigate(['message']).then(r => {
+    })
+
+  }
+}
